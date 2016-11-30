846f8596f284a9dfe90cd0bbc0b44726acbf1fff
Levi Franca <levi@ciandt.com>
2016-11-29 22:15:43 -0200

    Add production changes

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,22 +1,22 @@
-    angular.module('MeGuiaApp.directives', [])
-    .directive("myMaterialSelect", [ "$timeout", function($timeout) {
-        return {
-            restrict: 'A',
-            require : 'ngModel',
-            link : function(scope, element, attrs, ngModelCtrl) {
-                $(function() {
-                    $(element).material_select();
+angular.module('MeGuiaApp.directives', [])
+.directive("myMaterialSelect", [ "$timeout", function($timeout) {
+    return {
+        restrict: 'A',
+        require : 'ngModel',
+        link : function(scope, element, attrs, ngModelCtrl) {
+            $(function() {
+                $(element).material_select();
 
-                    $(element).change(function() {
-                        var value = $(element).val();
+                $(element).change(function() {
+                    var value = $(element).val();
 
-                        if (value) {
-                            var valueNum = new Number(value.substring(value.indexOf(':') + 1)) + 0;
+                    if (value) {
+                        var valueNum = new Number(value.substring(value.indexOf(':') + 1)) + 0;
 
-                            ngModelCtrl.$setViewValue(valueNum);
-                        }
-                    });
+                        ngModelCtrl.$setViewValue(valueNum);
+                    }
                 });
-            }
+            });
         }
-    }])
+    }
+}]);
